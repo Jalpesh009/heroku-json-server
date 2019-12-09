089646662943adcadb754ecc027df6657a3f19ef
Jalpesh Rajani <48354287+Jalpesh009@users.noreply.github.com>
2019-12-09 17:48:59 +0530

    added DB

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+
+server.use(middlewares);
+server.use('/api/v1', router)
+server.listen(3004, () => {
+  console.log("JSON Server is running");
+});
