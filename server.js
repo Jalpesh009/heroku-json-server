e8bf937bdc34e9712650ab8f0e8251dd5ccfd8e2
Hiren <Hiren.ghodasara@outlook.com>
2019-12-13 12:53:30 +0530

    add cros

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 const path = require("path");
+const cors = require("cors");
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 
 server.use(middlewares);
+server.use(cors());
 server.get("/test", (req, res) => {
   res.status(200).send({
-    text: "Hello!",
+    text: "Hello!"
   });
 });
-server.use('/api/v1', router);
+server.use("/api/v1", router);
 const port = process.env.PORT || 3004;
 server.listen(port, () => {
-  console.log("JSON Server is running",port);
+  console.log("JSON Server is running", port);
 });
